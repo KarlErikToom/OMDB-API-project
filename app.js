00f0a712e9341ac145068bc41cb563bf0018f4dd
Karl <toom.karl42@gmail.com>
2023-05-07 21:56:49 +0300

    still stuck on this

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+const movieSearchBox = document.querySelector(".search__input");
+const moviesListEl = document.querySelector(".movies");
 
-const movieSearchBox = document.querySelector(".search__input")
-
-async function loadMovies(searchTerm){
-    const movies = await fetch (`https://www.omdbapi.com/?s=${searchTerm}&apikey=a398627a`)
-    const moviesData = await movies.json()
-    console.log(moviesData)
+async function loadMovies(searchTerm) {
+  const movies = await fetch(
+    `https://www.omdbapi.com/?s=${searchTerm}&apikey=a398627a`
+  );
+  const moviesData = await movies.json();
+  moviesListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("");
 }
+loadMovies();
 
-function findMovies(){
-    let searchTerm = (movieSearchBox.value).trim()
-    console.log(searchTerm)
+/*function findMovies() {
+  let searchTerm = movieSearchBox.value.trim();
+  console.log(searchTerm);
+}*/
+function movieHTML(movie) {
+  return `<div class="movie">
+    <div class="movie__box--container">
+      <figure class="movie__img--wrapper">
+        <img class="movie__img" src="assets/batman.jfif" alt="" />
+      </figure>
+      <h2 class="movie__title">Movie Title: ${movie.title}</h2>
+      <p class="movie__date">Release Date : ${movie.year}</p>
+      <p class="movie__genre">Genre : action</p>
+      <p class="movie__director">Director: Matt Reeves</p>
+    </div>
+  </div>`;
 }

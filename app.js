99c8384269c9924f301bba9bff478479eccdecf9
Karl <toom.karl42@gmail.com>
2023-05-08 15:36:49 +0300

    added transition and show resultsfor

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const movieSearchBox = document.querySelector(".search__input");
 const moviesListEl = document.querySelector(".movies");
 const movieContainerEl = document.querySelector(".movie__box--container");
+const searchForm = document.querySelector(".search__form");
+
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let searchTerm = movieSearchBox.value.trim();
+  loadMovies(searchTerm);
+});
 
 async function loadMovies(searchTerm) {
   const movies = await fetch(
@@ -8,36 +15,38 @@ async function loadMovies(searchTerm) {
   );
   const moviesData = await movies.json();
   if (moviesData.Search) {
-    moviesListEl.innerHTML = moviesData.Search.map((movie) =>
-      movieHTML(movie)
+    const headerEl = document.querySelector(".movie__header")
+    headerEl.innerHTML = `Showing result for: ${searchTerm}`
+    moviesListEl.innerHTML = moviesData.Search.slice(0, 8).map((movie, index) =>
+      movieHTML(movie, index)
     ).join("");
-    moviesListEl
-      .querySelectorAll(".movie__box--container")
-      .forEach((container) => (container.style.display = "flex"));
+    const movieContainers = moviesListEl.querySelectorAll(
+      ".movie__box--container"
+    );
+    movieContainers.forEach((container, index) => {
+      container.style.animationDelay = `${(index + 1) * 200}ms`;
+      container.style.display = "flex"; 
+    });
   } else {
-    moviesListEl.innerHTML = "<h1>Mo movies found </h1>";
+    const headerEl = document.querySelector(".movie__header")
+    headerEl.innerHTML = `Showing results for: ${searchTerm}`
+    moviesListEl.innerHTML = "<h1>No movies found </h1>";
   }
+  
 }
 
-function findMovies(event) {
-  if (event.key === "Enter") {
-    let searchTerm = movieSearchBox.value.trim();
-    loadMovies(searchTerm);
-  }
-}
 function movieHTML(movie) {
-  return `<div class="movie">
-    <div class="movie__box--container">
-      <figure class="movie__img--wrapper">
-        <img class="movie__img" src="${movie.Poster}" alt="" />
-      </figure>
-      <h2 class="movie__title">${movie.Title}</h2>
-      <p class="movie__date">Release Date : ${movie.Year}</p>
-      <p class="movie__genre">Genre : action</p>
-      <p class="movie__director">Director: Matt Reeves</p>
+  return `
+    <div class="movie">
+      <div class="movie__box--container">
+        <figure class="movie__img--wrapper">
+          <img class="movie__img" src="${movie.Poster}" alt="" />
+        </figure>
+        <h2 class="movie__title">${movie.Title}</h2>
+        <p class="movie__date">Release Date : ${movie.Year}</p>
+        <p class="movie__genre">Type:${movie.Type} </p>
+        <p class="movie__director">Director: Matt Reeves</p>
+      </div>
     </div>
-  </div>`;
+  `;
 }
-movieSearchBox.addEventListener("keydown", function (event) {
-  findMovies(event);
-});

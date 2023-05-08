95bcbc88801a24339e7b37c52049b1a6897e9c40
Karl <toom.karl42@gmail.com>
2023-05-08 13:56:26 +0300

    finally got the search to work

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 const movieSearchBox = document.querySelector(".search__input");
 const moviesListEl = document.querySelector(".movies");
+const movieContainerEl = document.querySelector(".movie__box--container");
 
 async function loadMovies(searchTerm) {
   const movies = await fetch(
     `https://www.omdbapi.com/?s=${searchTerm}&apikey=a398627a`
   );
   const moviesData = await movies.json();
-  moviesListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("");
+  if (moviesData.Search) {
+    moviesListEl.innerHTML = moviesData.Search.map((movie) =>
+      movieHTML(movie)
+    ).join("");
+    moviesListEl
+      .querySelectorAll(".movie__box--container")
+      .forEach((container) => (container.style.display = "flex"));
+  } else {
+    moviesListEl.innerHTML = "<h1>Mo movies found </h1>";
+  }
 }
-loadMovies();
 
-/*function findMovies() {
-  let searchTerm = movieSearchBox.value.trim();
-  console.log(searchTerm);
-}*/
+function findMovies(event) {
+  if (event.key === "Enter") {
+    let searchTerm = movieSearchBox.value.trim();
+    loadMovies(searchTerm);
+  }
+}
 function movieHTML(movie) {
   return `<div class="movie">
     <div class="movie__box--container">
       <figure class="movie__img--wrapper">
-        <img class="movie__img" src="assets/batman.jfif" alt="" />
+        <img class="movie__img" src="${movie.Poster}" alt="" />
       </figure>
-      <h2 class="movie__title">Movie Title: ${movie.title}</h2>
-      <p class="movie__date">Release Date : ${movie.year}</p>
+      <h2 class="movie__title">${movie.Title}</h2>
+      <p class="movie__date">Release Date : ${movie.Year}</p>
       <p class="movie__genre">Genre : action</p>
       <p class="movie__director">Director: Matt Reeves</p>
     </div>
   </div>`;
 }
+movieSearchBox.addEventListener("keydown", function (event) {
+  findMovies(event);
+});
